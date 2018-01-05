77674d4c0131ed6e36f8d6043e787ae1143b8e5f
Jason Lin <jasonlin43212@gmail.com>
2018-01-04 20:41:37 -0500

    added a toString method to ConnectFour

--- ConnectFour.java
+++ ConnectFour.java
@@ -5,16 +5,18 @@ public class ConnectFour{
   private Piece[][] data;
   private Piece Player1;
   private Piece Player2;
+  private int width;
+  private int height;
   private boolean win;
   private boolean draw;
   private boolean isFirstPlayerTurn;
 
   //----------Methods------------
 
-  public ConnectFour (int x, int y, String playerOneColor, String playerTwoColor){
-    data = new Piece[x][y];
-    for (int i=0; i<x; i++){
-      for (int j=0; j<y; j++){
+  public ConnectFour (int width, int height, String playerOneColor, String playerTwoColor){
+    data = new Piece[width][height];
+    for (int i=0; i<width; i++){
+      for (int j=0; j<height; j++){
         data[i][j] = new Piece(0,"white");
       }
     }
@@ -38,6 +40,16 @@ public class ConnectFour{
     else {
       data[index][findNextColumn(index)] = Player2;
     }
+  }
+
+  public String toString () {
+    String output = "";
+    for (int y=height-1; y>=0; y--){
+      for (int x=0; x<width; x++){
+        output += data[x][y] + " "
+      }
+    }
+    return output;
   }
 
 }
