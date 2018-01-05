6e2224b727df88bde52e97c6b0af08a309b080cc
Karen Li <kli19@stuy.edu>
2018-01-04 21:12:54 -0500

    main for testing in Piece and skeleton for Rotatable Object

--- Piece.java
+++ Piece.java
@@ -1,25 +1,30 @@
 public class Piece{
 
-  //------Instance Variables--------
+    //------Instance Variables--------
 
-  private String color;
-  private int id;
+    private String color;
+    private int id;
 
-  //------Methods------
-  public Piece (int id, String color) {
-    this.color = color;
-    this.id = id;
-  }
+    //------Methods------
+    public Piece (int id, String color) {
+	this.color = color;
+	this.id = id;
+    }
 
-  public int getId () {
-    return id;
-  }
+    public int getId () {
+	return id;
+    }
 
-  public String toString () {
-    return "" + id;
-  }
+    public String toString () {
+	return "" + id;
+    }
 
-  public boolean equals (Piece other) {
-    return id == other.getId();
-  }
+    public boolean equals (Piece other) {
+	return id == other.getId();
+    }
+
+    public static void main(String[] args){
+	//Piece x = new Piece(0, "red");
+	//System.out.println(x.getId());
+    }
 }
