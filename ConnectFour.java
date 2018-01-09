5ecdacec628d86e77f0fc02215e205f5bc7b4153
kli19 <kli19@stuy.edu>
2018-01-09 15:31:42 -0500

    changed all if statements to while loops for future additions and fixed instructions

--- ConnectFour.java
+++ ConnectFour.java
@@ -13,7 +13,10 @@ public class ConnectFour extends JFrame implements /*ActionListener,*/ KeyListen
 
 	while (!stop){
 	    while (parameters.size() == 0){
-		System.out.println("Enter the HEIGHT of your board. \nThis should be an integer between 5 and 11, inclusive. ");
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n----------------------------\n"+
+				   "Enter the HEIGHT of your board."+
+				   "\n----------------------------\n"+
+				   "**This should be an integer between 5 and 11, inclusive.**\n");
 		String next = scan.next();
 		int h = 0;
 		try {
@@ -26,7 +29,11 @@ public class ConnectFour extends JFrame implements /*ActionListener,*/ KeyListen
 		}
 	    }
 	    while (parameters.size() == 1){
-		System.out.println("Enter the WIDTH of your board. \nThis should be an integer between 5 and 11, inclusive. ");
+		System.out.println("\n\n----------------------------\n"+
+				   "Enter the WIDTH of your board."+
+				   "\n----------------------------\n"+
+				   "**This should be an integer between 5 and 11, inclusive.**\n");
+
 		String next = scan.next();
 		int w = 0;
 		try {
@@ -39,27 +46,42 @@ public class ConnectFour extends JFrame implements /*ActionListener,*/ KeyListen
 		}
 	    }
 
-	    if (parameters.size() == 2){
-		System.out.println("Player 1: Enter your color");
+	    while (parameters.size() == 2){
+		System.out.println("\n\n----------------------------\n"+
+				   "Player 1: Enter your color"+
+				   "\n----------------------------\n");
 		String next = scan.next();
 		parameters.add(next);
 	    }
 
-	    if (parameters.size() == 3){
-		System.out.println("Player 2: Enter your color");
+	    while (parameters.size() == 3){
+		System.out.println("\n\n----------------------------\n"+
+				   "Player 2: Enter your color"+
+				   "\n----------------------------\n");
 		String next = scan.next();
 		parameters.add(next);
 	    }
 
-	    if (parameters.size() == 4){
-		stop = true;
-		System.out.println("You are done setting up! \n Now for some instructions: \n\n [<-]: moves arrow left \n" +
-                                    "[->]: moves arrow right \n" +
-                                    "[space]: drops piece\n" +
-                                    "[l]: rotates board left \n" +
-                                    "[r]: rotates board right \n"
+	    while (parameters.size() == 4){
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nYou are done setting up! \nNow for some instructions: \n\n" +
+				   "[<-]: moves arrow left \n" +
+				   "[->]: moves arrow right \n" +
+				   "[space]: drops piece\n" +
+				   "[q]: rotates board left \n" +
+				   "[e]: rotates board right \n" +
+				   "\n\n\n Enter \"OK\" to start!"
+			 
 				   );
+		String next = scan.next();
+		if (next.toLowerCase().equals("ok")){
+		    parameters.add(next);
+		}
 	    }
+
+	    if (parameters.size() == 5){
+		stop = true;
+	    }
+
 
 	}
 	
