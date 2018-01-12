6fa8805de80a9cc631f1a221fe1b7823dbe615a3
kli19 <kli19@stuy.edu>
2018-01-12 15:31:39 -0500

    can change background and text color in terminal

--- StartScreen.java
+++ StartScreen.java
@@ -1,5 +1,45 @@
 import java.util.*;
 public class StartScreen{
+        private static final int BRIGHT = 1;
+    private static final int DARK = 2;
+    private static final int ITALICS = 3;
+    private static final int BLACK = 30;
+    private static final int RED = 31;
+    private static final int GREEN = 32;
+    private static final int YELLOW = 33;
+    private static final int BLUE = 34;
+    private static final int MAGENTA = 35;
+    private static final int CYAN = 36;
+    private static final int WHITE = 37;
+    private static final String CLEAR_SCREEN =  "\033[2J";
+    private static final String HIDE_CURSOR =  "\033[?25l";
+    private static final String SHOW_CURSOR =  "\033[?25h";
+
+    private static final String RESET = color(40,37)+SHOW_CURSOR;
+
+    private static String color(int a, int b){
+        return ("\033[0;" + a+ ";" + b + "m");
+    }
+
+    private static String color(int a, int b,int c){
+        return ("\033[0;" + a+ ";" + b + ";" + c+ "m");
+    }
+    private static String color(int a, int b,int c, int d){
+        return ("\033[0;" + a+ ";" + b + ";" + c + ";" + d + "m");
+    }
+
+     public static int background(int color){
+	return color + 10;
+    }
+
+    //terminal specific character to move the cursor to a location
+    //top left is 1,1
+    private static String go(int x,int y){
+        return ("\033[" + x + ";" + y + "H");
+    }
+ 
+
+
     
     public static void main(String[]args){
 	boolean stop = false;
@@ -9,7 +49,9 @@ public class StartScreen{
 	while (!stop){
 
 	    while (parameters.size() == 0){
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n----------------------------\n"+
+		//System.out.println(go(i+1,j+19)+color(30+i,40+j,DARK,ITALICS) + "#");
+		System.out.println(go(1,1) + color(BLACK, background(WHITE), BRIGHT, ITALICS) +
+				   "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n----------------------------\n"+
 				   "Enter the HEIGHT of your board."+
 				   "\n----------------------------\n"+
 				   "**This should be an integer between 5 and 11, inclusive.**\n");
@@ -23,6 +65,7 @@ public class StartScreen{
 		if(h >= 5 && h <= 11){
 		    parameters.add(next);
 		}
+		System.out.println(RESET);
 	    }
 	    while (parameters.size() == 1){
 		System.out.println("\n\n----------------------------\n"+
@@ -78,5 +121,6 @@ public class StartScreen{
 	    }
 
 	}
+
     }
 }
