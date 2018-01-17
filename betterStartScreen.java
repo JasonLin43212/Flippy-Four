c27fe6c8d75f0f5abbd4318203947729fc6eb2d6
Karen Li <kli19@stuy.edu>
2018-01-16 20:37:00 -0500

    basic gui with text pane and button

--- betterStartScreen.java
+++ betterStartScreen.java
@@ -1,3 +1,48 @@
-public class betterStartScreen {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class betterStartScreen extends JFrame implements ActionListener{
+
+    private Container pane;
+    private JTextField text;
+    private JButton START;
+
+    
+    public betterStartScreen(){
+	this.setTitle("Flippy Four");
+	this.setSize(400,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+
+	text = new JTextField(5);
+	text.addActionListener(this);
+	pane.add(text);
+
+	START = new JButton("START");
+	START.addActionListener(this);
+	pane.add(START);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String str = e.getActionCommand();
+	System.out.println(str); 
+
+	if (str.equals("START")){
+	    //double result = FC(Double.parseDouble(text.getText()));
+	    //text.setText(result + "");
+	
+	}
+    }
+
+
+    public static void main(String[]args){
+	betterStartScreen x = new betterStartScreen();
+	x.setVisible(true);
+	
+    }
 }
