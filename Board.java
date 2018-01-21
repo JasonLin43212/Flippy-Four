8ac9b252bbb61f8f0e8842597db534a2183135e1
Jason Lin <jasonlin43212@gmail.com>
2018-01-21 17:42:37 -0500

    added comments to board

--- Board.java
+++ Board.java
@@ -4,10 +4,12 @@ public class Board extends RotatableObject {
 
   //---------------Methods---------------
 
+  //Constructor for Board
   public Board (int[] x, int[] y) {
     super(x,y);
   }
 
+  //Draws the board
   public void drawObject(Graphics g) {
     g.setColor(new Color(50,50,50));
     g.fillPolygon(xGui,yGui,x.length);
