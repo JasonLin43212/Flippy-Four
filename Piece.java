4a052fb054c922fb51e10a3895fe97fb7e723876
Karen Li <kli19@stuy.edu>
2018-01-21 17:27:38 -0500

    comments for functions/variables in piece

--- Piece.java
+++ Piece.java
@@ -2,39 +2,43 @@ import java.awt.*;
 
 public class Piece extends RotatableObject{
 
-  //---------------Instance Variables---------------
+    //---------------Instance Variables---------------
 
-  private Color color;
-  private int id;
+    private Color color;
+    private int id;
 
-  //---------------Methods---------------
+    //---------------Methods---------------
+   
+    //---------------Initialize rotation features, color, and ID---------------
+    public Piece (int id, Color color, int[] x, int[] y) {
+	super(x,y);
+	this.color = color;
+	this.id = id;
+    }
 
-  public Piece (int id, Color color, int[] x, int[] y) {
-    super(x,y);
-    this.color = color;
-    this.id = id;
-  }
+    //---------------Accessor Methods---------------
+    public int getId () {
+	return id;
+    }
 
-  public int getId () {
-    return id;
-  }
+    public String toString () {
+	return "" + id;
+    }
 
-  public String toString () {
-    return "" + id;
-  }
+    //---------------Implement Inherited Method---------------
+    public void drawObject(Graphics g) {
+	g.setColor(color);
+	g.fillOval(xGui[0],yGui[0],40,40);
+    }
 
-  public void drawObject(Graphics g) {
-    g.setColor(color);
-    g.fillOval(xGui[0],yGui[0],40,40);
-  }
+    //---------------Dropping Animation ---------------
+    public void drop() {
+	y[0] += 50;
+	yGui[0] += 50;
+    }
 
-  public void drop() {
-    y[0] += 50;
-    yGui[0] += 50;
-  }
-
-  public void startScreenDrop(){
-    y[0] += 5;
-    yGui[0] += 5;
-  }
+    public void startScreenDrop(){
+	y[0] += 5;
+	yGui[0] += 5;
+    }
 }
